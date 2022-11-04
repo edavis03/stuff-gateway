955527e247d8db7b979358b20d9731c5eceaedf2
Emily Davis <demily@vmware.com>
2022-11-04 13:28:38 -0400

    initial gateway stuff

---
+++ src/main/java/com/example/stuffgateway/StuffGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.stuffgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuffGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StuffGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stuffgateway/StuffGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stuffgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuffGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
