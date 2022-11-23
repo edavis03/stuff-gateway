04203d68814a3e43ecf2b381ffc28b5d5fcc3a03
Lindsey Gillaspie <lgillaspie@lgillaspie-a02.vmware.com>
2022-11-23 15:01:26 -0500

    Update security config to include PKCE

--- src/main/java/com/example/stuffgateway/StuffGatewayApplication.java
+++ src/main/java/com/example/stuffgateway/StuffGatewayApplication.java
@@ -6,29 +6,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestCustomizers;
+import org.springframework.security.oauth2.client.web.server.DefaultServerOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizationRequestResolver;
 
 @SpringBootApplication
 public class StuffGatewayApplication {
 
-	@Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http,
-		ReactiveClientRegistrationRepository clientRegistrationRepository) {
-      // Authenticate through configured OpenID Provider
-      http.oauth2Login();
-    
-      // Also logout at the OpenID Connect provider
-    //   http.logout(logout -> logout.logoutSuccessHandler(
-    //     new OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository)));
-    
-      // Require authentication for all requests
-      http.authorizeExchange().anyExchange().authenticated();
-    
-      // Allow showing /home within a frame
-    //   http.headers().frameOptions().mode(Mode.SAMEORIGIN);
-    
-      // Disable CSRF in the gateway to prevent conflicts with proxied service CSRF
-      http.csrf().disable();
-      return http.build();
+    @Bean
+    public SecurityWebFilterChain pkceFilterChain(ServerHttpSecurity http, ServerOAuth2AuthorizationRequestResolver resolver) {        
+        http.authorizeExchange(r -> r.anyExchange().authenticated());
+        http.oauth2Login(auth -> auth.authorizationRequestResolver(resolver));
+        return http.build();
+    }
+
+    @Bean
+    public ServerOAuth2AuthorizationRequestResolver pkceResolver(ReactiveClientRegistrationRepository repo) {
+        DefaultServerOAuth2AuthorizationRequestResolver resolver = new DefaultServerOAuth2AuthorizationRequestResolver(repo);
+        resolver.setAuthorizationRequestCustomizer(OAuth2AuthorizationRequestCustomizers.withPkce());
+        return resolver;
     }
 
 	public static void main(String[] args) {
