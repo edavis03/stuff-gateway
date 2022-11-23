eded21f9cc92c37e176fab95018bf4fd5713e804
Lindsey Gillaspie <lgillaspie@lgillaspie-a02.vmware.com>
2022-11-23 11:49:13 -0500

    Add local and sso security configs

---
+++ src/main/java/com/example/stuffgateway/SsoConfig.java
@@ -0,0 +1,31 @@
+package com.example.stuffgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestCustomizers;
+import org.springframework.security.oauth2.client.web.server.DefaultServerOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizationRequestResolver;
+
+@Configuration
+@Profile("sso")
+public class SsoConfig {
+      
+  @Bean
+  public SecurityWebFilterChain pkceFilterChain(ServerHttpSecurity http, ServerOAuth2AuthorizationRequestResolver resolver) {        
+      http.authorizeExchange(r -> r.anyExchange().authenticated());
+      http.oauth2Login(auth -> auth.authorizationRequestResolver(resolver));
+      return http.build();
+  }
+
+  @Bean
+  public ServerOAuth2AuthorizationRequestResolver pkceResolver(ReactiveClientRegistrationRepository repo) {
+      DefaultServerOAuth2AuthorizationRequestResolver resolver = new DefaultServerOAuth2AuthorizationRequestResolver(repo);
+      resolver.setAuthorizationRequestCustomizer(OAuth2AuthorizationRequestCustomizers.withPkce());
+      return resolver;
+  }
+
+}
