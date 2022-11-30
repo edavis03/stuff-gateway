0b0d33babbe9becfe2dbc02b3029015b634af5d2
Lindsey Gillaspie <lgillaspie@lgillaspie-a01.vmware.com>
2022-11-30 13:59:40 -0500

    Variable-ize the api and ui routes

--- src/main/java/com/example/stuffgateway/SsoConfig.java
+++ src/main/java/com/example/stuffgateway/SsoConfig.java
@@ -3,6 +3,8 @@ package com.example.stuffgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
